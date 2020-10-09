c28a36904ba4d5ebdbf3f5547ce656487146a1d1
abruno92 <abruno92@github.com>
2020-10-09 09:39:18 +0200

    fixed the numbers

--- si_mandatory_assignment_1/Main_System/Legacy_Central_System.py
+++ si_mandatory_assignment_1/Main_System/Legacy_Central_System.py
@@ -3,36 +3,34 @@ import random as rand
 import numpy as np
 import json
 import requests
-
-col_list = ['FirstName', 'LastName', 'DateOfBirth', 'Email',]
-
-symbol = ('-') 
-
-number = str(rand.randint(1111, 9999))
+import os.path 
 
 def read_data(file, list):
     data = pd.read_csv(file, usecols=col_list)
     return data
 
-
-def create_cpr(df):
-    df['CprNumber'] = df['DateOfBirth'].str.replace('-', '') + symbol + number
-    df = df.drop(['DateOfBirth'], axis=1)
+def create_cpr(df, symbol):
+    number = np.random.randint(1111, 9999, size=len(df)).astype(str)
+    df['CprNumber'] = df.pop('DateOfBirth').str.replace('-', '') + symbol + number
     return df
-    
 
 def create_xml(df):
-    xml = ['<Person>']
-    for field in df.index:
-        xml.append('  <field name="{0}">{1}</field>'.format(field, df[field]))
-    xml.append('</Person>')
+    shema = 'Person'
+    xml = ["<{}>".format(shema)]
+    for data in df.index:
+        xml.append('  <field name="{0}">{1}</field>'.format(data, df[data]))
+    xml.append("</{}>".format(shema))
     return '\n'.join(xml)
-    
 
 if __name__ == "__main__":
-    df = create_cpr(read_data('si_mandatory_assignment_1/Main_System/people.csv', col_list))
-    print('\n'.join(df.apply(create_xml, axis=1)))
-    
-    df1 = df[['CprNumber', 'Email']]
-    person = df1.to_json(r'people.json', orient="records")
-    requests.post('http://localhost:8088/generate-nemID', json=person)
+    col_list = ['FirstName', 'LastName', 'DateOfBirth', 'Email']
+    symbol = ('-') 
+    base_url = "http://127.0.0.1:8080"
+
+    df = create_cpr(read_data('NemID-Project/si_mandatory_assignment_1/Main_System/people.csv', col_list), symbol)
+    xml_person = '\n'.join(df.apply(create_xml, axis=1))
+
+    headers = {'Content-Type': 'application/xml'}
+    response = requests.post("{}/nemId".format(base_url), xml_person, headers=headers).text
+
+    print(response)
