2ccbf206ba9adc748113cb19bdaeae7ca0cbf7ff
abruno92 <abruno92@github.com>
2020-10-10 10:14:07 +0200

    fixed legacy

--- si_mandatory_assignment_1/Main_System/Legacy_Central_System.py
+++ si_mandatory_assignment_1/Main_System/Legacy_Central_System.py
@@ -3,7 +3,8 @@ import random as rand
 import numpy as np
 import json
 import requests
-import os.path 
+from os.path import dirname, abspath
+from pip._vendor import msgpack
 
 def read_data(file, list):
     data = pd.read_csv(file, usecols=col_list)
@@ -16,7 +17,9 @@ def create_cpr(df, symbol):
 
 def create_xml(df):
     shema = 'Person'
-    xml = ["<{}>".format(shema)]
+    version = "1.1"
+    xml = ['<?xml version="{}" encoding="UTF-8"?>'.format(version)]
+    xml.append("<{}>".format(shema))
     for data in df.index:
         xml.append('  <field name="{0}">{1}</field>'.format(data, df[data]))
     xml.append("</{}>".format(shema))
@@ -25,12 +28,20 @@ def create_xml(df):
 if __name__ == "__main__":
     col_list = ['FirstName', 'LastName', 'DateOfBirth', 'Email']
     symbol = ('-') 
-    base_url = "http://127.0.0.1:8080"
+    cwd = dirname(dirname(abspath(__file__)))
+    base_url = 'http://127.0.0.1:8080'
 
-    df = create_cpr(read_data('NemID-Project/si_mandatory_assignment_1/Main_System/people.csv', col_list), symbol)
-    xml_person = '\n'.join(df.apply(create_xml, axis=1))
+    df = create_cpr(read_data('{}/people.csv'.format(cwd), col_list), symbol)
+    person = '\n'.join(df.apply(create_xml, axis=1))
+    print(person)
 
     headers = {'Content-Type': 'application/xml'}
-    response = requests.post("{}/nemId".format(base_url), xml_person, headers=headers).text
+    response = requests.post("{}/nemId".format(base_url), data=person, headers=headers).text
+
+    person.nemID = json.loads(response.content)["nemID"]
+
+    with open('{}/msgpack_files/{}.msgpack'.formart(cwd, person.cpr), "wb") as outfile:
+        packed = msgpack.packb(person.__dict__)
+        outfile.write(packed)
 
     print(response)
