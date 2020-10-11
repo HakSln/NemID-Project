bc100044c5e44124d5860ac4457e7f27d6247513
Hakan Aslan <33924602+Hakan2703@users.noreply.github.com>
2020-10-11 11:21:52 +0200

    hotfix

--- si_mandatory_assignment_1/NemID_CodeGenerator/routes/index.js
+++ si_mandatory_assignment_1/NemID_CodeGenerator/routes/index.js
@@ -1,38 +1,53 @@
-const { query } = require('express');
 var express = require('express');
 var router = express.Router();
 
  //Check against the data from the database.
  //If it matches this will return a JSON bodywith status code 200. Otherwise it will return a 403(forbidden)
 
-const users[] = query('SELECT * FROM auth_log WHERE');
+//const users[] = query('SELECT * FROM auth_log WHERE');
 /* POST */
 router.post('/', (req, res) => {
 
-connection.connect();
-
-users=[];
-
-app.get('/', function(req, res){
-    connection.query(
-    'SELECT * FROM auth_log WHERE nemId = [req.body.nemId]',
-    function(err, result)
-    {
-        if(err) throw err;
-        res.render('test',{nemId:"", users:result});
-        aUser = result;
-    });
-});
-
-
     const random_code =  Math.floor(Math.random() * 899999 + 100000)
-    const user = users.find(user.nemId, user.nemIdCode = req.body.nemId, req.body.nemIdCode) 
-    if(user == null){
-        return res.statusCode(403)
-    } else{
+
+    var myNemId = req.body.NemId;
+
+req.connection(function(err, connection)
+{
+    if (err) throw err;     
+    connection.query("SELECT NemID FROM user WHERE NemId =" + myNemId, 
+    function(results) {    
+    if (results) {
         res.statusCode(200)
         res.json(random_code)
-    }
+    } 
+    else{
+        return res.statusCode(403)}
+    });
+})
+
+// users=[]= query('SELECT * FROM auth_log WHERE ');
+//var user = users.find(user.nemId, user.nemIdCode = req.body.nemId, req.body.nemIdCode
+//app.get('/', function(req, res){
+//    connection.query(
+//    'SELECT * FROM auth_log WHERE nemId = [req.body.nemId]',
+//    function(err, result)
+//    {
+//        if(err) throw err;
+//        res.render('test',{nemId:"", users:result});
+//        aUser = result;
+//    });
+//});
+
+
+    
+    //const user = users.find(user.nemId, user.nemIdCode = req.body.nemId, req.body.nemIdCode) 
+    //if(user == null){
+    //    return res.statusCode(403)
+    //} else{
+    //    res.statusCode(200)
+    //    res.json(random_code)
+   // }
 } 
 );
 module.exports = router;
