6a15d03ed742b6ae369d642e74c3fc5d8ce6e5ec
abruno92 <abruno92@github.com>
2021-01-11 13:37:30 +0100

    bascially redid all hakans code

---
+++ si_mandatory_assignment_1/Main_System/Legacy_Central_System_Python.py
@@ -0,0 +1,67 @@
+import pandas as pd
+import random as rand
+import numpy as np
+import json
+import os
+import requests
+import xml.etree.ElementTree as ET
+import msgpack
+
+from pathlib import Path
+
+from string import Template
+
+symbol = ('-') 
+file = 'people.csv'
+cwd = Path(os.path.dirname(__file__))
+path = cwd/f"{file}"
+headers = {'Content-Type': 'text/xml', 'Accept':'application/xml'}
+esb_serivce_address = 'http://127.0.0.1:8080'
+esb_serivce_endpoint = 'nemID'
+
+
+# Read columns specified in csv files and then turn data into dataframe
+def read_data(file, list):
+    data = pd.read_csv(file, usecols=col_list)
+    return data
+
+# Create 4 digit random number for the length of rows in the df and then cast to string
+# Then populate new CprNumber column with values from DateofBirth, replace the symbol 
+# with no space and then add new symbol followed by the random numbers generated
+def create_cpr(df, symbol):
+    number = np.random.randint(1111, 9999, size=len(df)).astype(str)
+    df['CprNumber'] = df['DateOfBirth'].str.replace('-', '') + symbol + number
+    return df
+
+# Create xml with specific elements from df
+def create_xml(firstname, lastname, email, cpr):
+    person = ET.Element('Person')
+    ET.SubElement(person, 'FirstName').text = firstname
+    ET.SubElement(person, 'LastName').text = lastname
+    ET.SubElement(person, 'Email').text = email
+    ET.SubElement(person, 'CprNumber').text = cpr
+    return ET.tostring(person)
+
+if __name__ == "__main__":
+    col_list = ['FirstName', 'LastName', 'DateOfBirth', 'Email', 'Phone', 'Address', 'Country']
+
+    df = create_cpr(read_data(f'{path}', col_list), symbol)
+    print('df=', df)
+
+    for person in df.values.tolist():
+        firstname = person[0] 
+        lastname = person[1]
+        email = person[2] 
+        cpr  = person[7]  
+
+    person_xml = create_xml(firstname, lastname, email, cpr)
+    
+    response = requests.post(f"{esb_serivce_address}/{esb_serivce_endpoint}", data=person_xml, headers=headers)
+    
+    nemID = json.loads(response.text)["nemID"]
+
+    person.append(nemID)
+
+    with open(f'{cwd}/msgpack/{cpr}.msgpack', "wb") as outfile:
+           packed = msgpack.packb(person)
+           outfile.write(packed)
